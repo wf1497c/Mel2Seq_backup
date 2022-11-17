5286b53f8be221a02cb78b3b2a6a19a3a90fd7e1
HY Tang <wf1497b@gmail.com>
2022-11-16 21:30:41 -0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,13 +2,13 @@
 
 wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 ./awscli-bundle/install -b ~/bin/aws
-./awscli-bundle/install -h
+echo $PATH | grep ~/bin
+export PATH=~/bin:$PATH
 
 aws configure set aws_access_key_id AKIAZPK3ZT3PUITBP4OK
 aws configure set aws_secret_access_key 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
 aws configure set region ap-south-1
 aws s3 cp s3://wf1497bbucket/test . --recursive
 
-pip install librosa, transformers, matplotlib, tqdm --quiet
+pip install librosa transformers matplotlib tqdm --quiet
