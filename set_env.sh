bbb103b3bd6b769b570bd8e73ab95caf352e8f6a
HY Tang <wf1497b@gmail.com>
2022-11-16 22:09:30 -0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -6,9 +6,9 @@ unzip awscli-bundle.zip
 echo $PATH | grep ~/bin
 export PATH=~/bin:$PATH
 
-aws configure set aws_access_key_id AKIAZPK3ZT3PUITBP4OK
-aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id AKIAZPK3ZT3P4EPBKQ7P
+aws configure set aws_secret_access_key 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
 aws configure set region ap-south-1
-aws s3 cp s3://wf1497bbucket/test . --recursive
+aws s3 cp s3://wf1497bbucket/Mel2Seq/project/ . --recursive
 
 pip install librosa transformers matplotlib tqdm --quiet
